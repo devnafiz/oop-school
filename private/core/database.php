ab5fb80002db6b458dec3ddcab9e3084a46f647e
nafiz <nafiz.2011@yahoo.com>
2022-08-02 09:51:12 +0600

    add database funtion

---
+++ private/core/database.php
@@ -0,0 +1,54 @@
+<?php
+
+
+  /**
+   * database connection
+   */
+  class Database 
+  {
+  	
+  	private function connect()
+  	{
+  		$string ="mysql:host=localhost;dbname=school_db";
+  		if(!$con = new PDO($string,'root','')){
+
+  			die("Could not  connect database");
+  		}
+
+  		return $con;
+  	}
+
+  	public function run($query,$data=array(),$data_type = "object"){
+
+        $con =$this->connect();
+        $stm =$con->prepare($query);
+        if($stm){
+          $check = $stm->execute($data);
+
+          if($check){
+
+          	 if($data_type =="object"){
+          	 	$data = $stm->fetchAll(PDO::FETCH_OBJ);
+
+          	 }else{
+          	 	$data = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+          	 }
+
+          	 if(is_array($data) && count($data)>0){
+
+          	 	return $data;
+          	 }
+
+          	 
+          }
+
+        }
+        return false;
+  	}
+
+  	public function query(){
+
+
+  	}
+  }
