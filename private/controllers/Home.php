645e9f7d17a6ac7098b74f5cb3670284bb4b675b
nafiz <nafiz.2011@yahoo.com>
2022-08-05 16:10:41 +0600

    update funtion add

--- private/controllers/Home.php
+++ private/controllers/Home.php
@@ -9,6 +9,15 @@ class Home extends Controller
 	{
 		 //$user =$this->load_model('User');
 		$user = new User();
+		    $arr['firstname'] = 'bbom2';
+           // $arr['lastname'] = 'ggo';
+           // $arr['gender'] = 'male';
+            //$arr['date'] = '2022-08-02 20:03:12';
+            //$arr['user_id'] = '44444444';
+           /// $arr['school_id'] = '66';
+           // $arr['rank'] = 'student';
+            //$user->insert($arr);
+           $user->update(3,$arr);
 		 $data=$user->findAll();
 		 //$data =$user->where('firstname','sfdsd');
 		 $this->view('home',['rows'=>$data]);
--- private/core/model.php
+++ private/core/model.php
@@ -36,4 +36,45 @@ class Model extends Database
       return $this->query($query);
       
 	}
+
+	public function insert($data){
+
+          $keys =array_keys($data);
+          $columns =implode(',', $keys);
+          //var_dump( $columns);
+           $value =implode(',:', $keys);
+
+         $query="insert into $this->table ($columns) values(:$value)";
+
+      return $this->query($query,$data);
+	}
+	public function update($id,$data){
+		//print_r($data);
+        $data['id']=$id;
+        //print_r($data['id']);
+        $str="";
+        foreach ($data as $key => $value) {
+        	$str .=$key."=:".$key.",";
+        }
+
+		 
+         $str =trim($str,",");
+          //print_r( $str);
+        
+
+        
+
+      $query ="update  $this->table set $str where id =:id";
+      return $this->query($query,$data);
+	}
+	public function delete($id){
+		$column =addcslashes($column);
+
+      $query ="select * from $this->table where $column = :value";
+      return $this->query($query,[
+
+          
+          'value'=>$value
+      ]);
+	}
 }
