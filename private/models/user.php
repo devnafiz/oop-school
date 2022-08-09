b70a3ffab6a670e7d8fa6c59ed6680cc002f9cff
nafiz <nafiz.2011@yahoo.com>
2022-08-09 10:26:35 +0600

    add  password check

--- private/models/user.php
+++ private/models/user.php
@@ -9,15 +9,44 @@ class User extends Model
 {
 	
 	public function validate($DATA){
-		$this->errors=array();
+		$this->errors = array();
 
-		if($DATA['password'] != $DATA['password2']){
+		if(empty($DATA['firstname']) || !preg_match('/^[a-zA-A]+$/', $DATA['firstname'])){
 
-			$this->errors[]= "This password don't match";
+			$this->errors['firstname']="Only data letter  allaow";
+		}
+		if(empty($DATA['lastname']) || !preg_match('/^[a-zA-A]+$/', $DATA['lastname'])){
+
+			$this->errors['lastname']="Only data letter  allaow";
+		}
+
+		if(empty($DATA['email']) || !filter_var($DATA['email'],FILTER_VALIDATE_EMAIL)){
+
+			$this->errors['email']="Email not emapty ";
+		}
+		//gender 
+		$gender =['female','male'];
+
+		if(empty($DATA['gender']) || !in_array($DATA['gender'],$gender)){
+
+			$this->errors['gender']= "Gender not valid";
 
 		}
 
-		if(count($this->errors)==0){
+		if(empty($DATA['password']) || $DATA['password'] != $DATA['password2']){
+
+			$this->errors['password']= "This password don't match";
+
+		}
+
+		//password length
+		if(  strlen($DATA['password']) <=8 ){
+
+			$this->errors['password']= "This password  mast be 8 cheracter";
+
+		}
+
+		if(count($this->errors) == 0){
           return true;
 		}
 
--- private/views/sign_up.view.php
+++ private/views/sign_up.view.php
@@ -2,29 +2,33 @@
 	
   <?php  $this->view('includes/header');?>
 
+  <?php
+     print_r($errors);
+  ?>
+
 	<div class="conteiner-fluid">
 		<form action="" method="post">
 		 <div class="p-4 mx-auto shadow" style="max-width: 320px; width: 100% ;margin-top: 30px">
 		 	<h2 class="text-center">My school</h2>
 		 	<h3 class="text-center"> Add User</h3>
-             <input type="text" name="firstname" class="my-2 form-control" placeholder="First name" >
-		 	 <input type="text" name="lastname" class="my-2 form-control" placeholder="Last Name" >
-		 	 <input type="text" name="email" class="my-2 form-control" placeholder="Email" >
+             <input type="text" name="firstname" <?=get_var('firstname')?> class="my-2 form-control" placeholder="First name" >
+		 	 <input type="text" name="lastname" <?=get_var('lastname')?> class="my-2 form-control" placeholder="Last Name" >
+		 	 <input type="text" name="email" <?=get_var('email')?> class="my-2 form-control" placeholder="Email" >
 		 	 <select class=" my-2 form-control" name="gender">
-		 	 	<option value="">--Select gender</option>
-		 	 	<option value="male">Male</option>
-		 	 	<option value="female">Female</option>
+		 	 	<option <?=get_select('gender','')?> value="">--Select gender</option>
+		 	 	<option <?=get_select('gender','male')?> value="male">Male</option>
+		 	 	<option <?=get_select('gender','female')?> value="female">Female</option>
 		 	 </select>
 		 	 <select class="my-2 form-control" name="rank" >
-		 	 	 <option value="">--Select Rank--</option>
-		 	 	 <option value="Student">Student</option>
-		 	 	 <option value="Reception">Reception</option>
-		 	 	 <option value="Lecturer">Lecturer</option>
-		 	 	 <option value="Admin">Admin</option>
-		 	 	 <option value="Super admin">Super Admin</option>
+		 	 	 <option <?=get_select('rank','')?> value="">--Select Rank--</option>
+		 	 	 <option <?=get_select('rank','Student')?> value="Student">Student</option>
+		 	 	 <option <?=get_select('rank','Reception')?> value="Reception">Reception</option >
+		 	 	 <option <?=get_select('rank','Lecturer')?> value="Lecturer">Lecturer</option >
+		 	 	 <option <?=get_select('rank','Admin')?> value="Admin">Admin</option>
+		 	 	 <option <?=get_select('rank','Super admin')?> value="Super admin">Super Admin</option>
 		 	 </select>
-		 	 <input type="text" name="password" class=" my-2 form-control" placeholder="password" >
-		 	 <input type="text" name="password2" class=" my-2 form-control" placeholder="confirm password" >
+		 	 <input type="text"  <?=get_var('password')?>  name="password" class=" my-2 form-control" placeholder="password" >
+		 	 <input type="text"  <?=get_var('password2')?>  name="password2" class=" my-2 form-control" placeholder="confirm password" >
 		 	  <br>
 		 	  <button class="btn btn-info" type="submit">Sign Up</button>
 		 </div>	
